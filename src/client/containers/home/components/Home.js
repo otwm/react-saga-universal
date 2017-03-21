af8062bb9336b51ae6de3a94145d265e72c53aa7
kim-dong-o <otwm@yanolja.in>
2017-03-21 16:53:54 +0900

    actions 제거

--- src/client/containers/home/components/Home.js
+++ src/client/containers/home/components/Home.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router';
-import { toggleTodo } from '/actions/todos';
+import { toggleTodo } from '../core/actions';
 if(process.env.WEBPACK) require('./index.scss');
 
 class Home extends Component {
